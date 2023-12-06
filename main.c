3d8e873b2d29f9c79c62d49d1717b06b22213dd2
maxgraffv <maksymilian.graff@gmail.com>
2023-12-06 23:18:27 +0100

    Created main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+			 	/* *p -> actual halue of addres it points to
+				    p -> address it points to
+				   &p -> address of the pointer itself */
+
+int main()
+{
+	/* Program open
+		Check User list
+		If no users -> create a user account
+	
+		If users -> log into an existing account or create new one
+	
+	When Logged into
+		Options:
+		-check balance
+		-make transfer (Only to other account on the list)
+		-check transfer history (Sort by money / Sort by date )
+		-settings (change user data)
+			- in settings -
+			change user data
+			delete account (if have money -> transfer to other account ?
+						 otherwise goes to the bank) */
+
+
+}
